492536d52c3554cfc6c1dfbd0f0634d3808b06b0
Lais Faustino <laisfaustino@hotmail.com>
2023-03-28 13:08:49 -0300

    Add files via upload

--- TestaGame.java
+++ TestaGame.java
@@ -1,16 +1,26 @@
-package exercio03;
+package exercicio03;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class TestaGame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		LocalDate data =  LocalDate.of(2020, 9 ,20);
-		Produto g1 = new Produto(1,"Valorant",1, "Riot Games",data,1200.0f);
 		
+		// Formatador de datas - PT-BR
+		DateTimeFormatter df = DateTimeFormatter.ofPattern("d/MM/yyyy");
+		
+		LocalDate data1 = LocalDate.of(2020, 9, 20);		
+		Produto g1 = new Produto(1, "Valorant", 1, "Riot Games", data1, 1200.0f);
 		g1.visualizar();
+		
+		LocalDate data2 = LocalDate.parse("2021-08-15");
+		Console cs1 = new Console(2, "X-Box 360", 2, "Microsoft", data2, 5000.00f, 1);
+		cs1.visualizar();
+		
+		LocalDate data3 = LocalDate.parse("15/10/2022", df);
+		Jogo jg1 = new Jogo(3, "Kena", 2, "Microsoft", data3, 600.00f, 1);
+		jg1.visualizar();
 		
 	}
 
